cf840e37df2e1a8d24a4f519ab19e90680bbc56c
JeremyPP <jimypougnet@gmail.com>
2016-09-01 16:38:42 +0200

    initial implementation of the More details panel toggler

--- js/perso_dashboard.js
+++ js/perso_dashboard.js
@@ -50,22 +50,40 @@ $('.searchFieldInput').blur(function() {
 });
 
 // Filter personalization cards
-    $(".searchFieldInput").keyup(function(){
- 
-        // Retrieve the input field text and reset the count to zero
-        var filter = $(this).val(), count = 0;
- 
-        // Loop through the list
-        $(".filteredCard").each(function(){
- 
-            // If the list item does not contain the text phrase hide it
-            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-                $(this).hide();
- 
-            // Show the list item if the phrase matches and increase the count by 1
-            } else {
-                $(this).show();
-                count++;
-            }
-        });
-    });
+$(".searchFieldInput").keyup(function(){
+
+	// Retrieve the input field text and reset the count to zero
+	var filter = $(this).val(), count = 0;
+
+	// Loop through the list
+	$(".filteredCard").each(function(){
+
+		// If the list item does not contain the text phrase hide it
+		if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+			$(this).hide();
+
+		// Show the list item if the phrase matches and increase the count by 1
+		} else {
+			$(this).show();
+			count++;
+		}
+	});
+});
+
+
+// Open more details
+
+// Need better implementation so two containers can both stay open at the same time
+var toggled = false;
+
+$(".perso-card").on("click", ".perso-card-ButtonContainer", function(){
+	if( toggled ) {
+		$(this).next().hide();
+		$(this).children().children("md-icon").html("<md-icon> keyboard_arrow_down </md-icon>");
+		toggled = false
+	} else {
+		$(this).next().show();
+		$(this).children().children("md-icon").html("<md-icon> keyboard_arrow_up </md-icon>")
+		toggled = true
+	}
+}); 
